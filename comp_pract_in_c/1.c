cd2dd5ad59f7d8c355e0fd55fbd5871a3f7eacdf
PrxBxny <konotop.ilya1304@gmail.com>
2026-09-11 13:36:25 +0300

    add first task

---
+++ comp_pract_in_c/1.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+
+bool is_same(int num) /* если к сумме цифр этого числа прибавить квадрат этой суммы, то получится это же число */
+{
+    if (num < 10 || num >= 100)
+        return false; /* не подходит по условию */
+
+    int first_num = num / 10;
+    int second_num = num % 10;
+    int sum = first_num + second_num;
+
+    if (num == first_num + second_num + (sum * sum))
+        return true;
+
+    return false;
+}
+
+int main()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        if (is_same(i))
+            printf("%d\n", i);
+    }
+    
+    return 0;
+}
