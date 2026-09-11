6afc4532e4006297516a01ee6f73a5c64ad12c2b
PrxBxny <konotop.ilya1304@gmail.com>
2026-09-11 13:37:53 +0300

    add third task

---
+++ comp_pract_in_c/3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "arrio.h"
+#include "arr_processing.h"
+
+#define SIZE 19
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int arr[SIZE];
+    input_arr(arr, SIZE);
+    print_arr(arr, SIZE);
+
+    int sum = sumNegativMultipleOfFour(arr, 19);
+
+    printf("сумма отрицательных чисел делящихся на 4: %d\n", sum);
+
+    return 0;
+}
